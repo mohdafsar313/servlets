581d515c8d76800ed3ef7cbb1c89c135b4010076
afsar <afsarappuz07@gmail.com>
2025-05-14 13:22:16 +0530

    aadhar servlet commit

---
+++ hospital/src/servlet/ContactServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class ContactServlet extends GenericServlet {
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("running service in ContactServlet");
+
+        String firstname=servletRequest.getParameter("firstname");
+
+        String lastname=servletRequest.getParameter("lastname");
+
+        String email=servletRequest.getParameter("email");
+
+        String age=servletRequest.getParameter("message");
+
+    }
+}
