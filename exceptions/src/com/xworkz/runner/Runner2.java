f2f289a9422913a6cf99b6129316d9014ce19abb
afsar <afsarappuz07@gmail.com>
2025-06-12 08:22:17 +0530

    commit on 12-6-25

---
+++ exceptions/src/com/xworkz/runner/Runner2.java
@@ -0,0 +1,42 @@
+package com.xworkz.runner;
+
+import com.xworkz.exception.RuntimeEx;
+
+public class Runner2 {
+    public static void main(String[] args) {
+        RuntimeEx runtimeEx = new RuntimeEx();
+        runtimeEx.getArithmeticException();
+        runtimeEx.getArrayStoreException();
+        runtimeEx.getBufferOverflowException();
+        runtimeEx.getBufferUnderflowException();
+        runtimeEx.getCannotRedoException();
+        runtimeEx.getCannotUndoException();
+        runtimeEx.getClassCastException();
+        runtimeEx.getConcurrentModificationException();
+        runtimeEx.getEmptyStackException();
+        runtimeEx.getFileSystemAlreadyExistsException();
+        runtimeEx.getFileSystemNotFoundException();
+        runtimeEx.getIllegalArgumentException();
+        runtimeEx.getIllegalStateException();
+        runtimeEx.getIllegalMonitorStateException();
+        runtimeEx.getIllformedLocaleException();
+        runtimeEx.getIndexOutOfBoundsException();
+        runtimeEx.getJMRuntimeException();
+        runtimeEx.getMalformedParametersException();
+        runtimeEx.getMalformedParameterizedTypeException();
+        runtimeEx.getMissingResourceException();
+        runtimeEx.getNegativeArraySizeException();
+        runtimeEx.getNoSuchElementException();
+        runtimeEx.getNullPointerException();
+        runtimeEx.getProviderException();
+        runtimeEx.getRejectedExecutionException();
+        runtimeEx.getSecurityException();
+        runtimeEx.getUnmodifiableSetException();
+        runtimeEx.getUnsupportedOperationException();
+        runtimeEx.getWebServiceException();
+        runtimeEx.getWrongMethodTypeException();
+
+    }
+
+
+}
